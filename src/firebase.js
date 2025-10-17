9fbf9435ae5745996076cb6d8eb0395230edbab2
Shashank Reddy Mopuru <139841928+shashankmopuru@users.noreply.github.com>
2025-10-17 09:23:12 +0530

    Add files via upload

---
+++ src/firebase.js
@@ -0,0 +1,64 @@
+import { initializeApp } from "firebase/app";
+import {
+  GoogleAuthProvider,
+  signInWithPopup,
+  getAuth,
+  signOut,
+} from "firebase/auth";
+import Cookies from "js-cookie";
+import { setDoc, doc, serverTimestamp, getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+import { useNavigate } from "react-router-dom";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: "ar-fitness3.firebaseapp.com",
+  projectId: "ar-fitness3",
+  storageBucket: "ar-fitness3.appspot.com",
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGE_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
+
+export const signInWithGoogle = async () => {
+  try {
+    const googleProvider = new GoogleAuthProvider();
+    const res = await signInWithPopup(auth, googleProvider);
+
+    const accessToken = res.user.accessToken;
+    Cookies.set("uat", accessToken);
+    const uid = res.user.uid.toString();
+    Cookies.set("userID", uid);
+
+    const name = res.user.displayName;
+    const email = res.user.email;
+    const photo = res.user.photoURL;
+
+    localStorage.setItem("name", name);
+    localStorage.setItem("email", email);
+    localStorage.setItem("photo", photo);
+    const docRef = doc(db, "user", uid);
+    await setDoc(docRef, {
+      userID: uid,
+      timeStamp: serverTimestamp(),
+      name: res.user.displayName,
+    });
+  } catch (err) {
+    console.error(err);
+    alert(err.message);
+  }
+};
+
+//   Logout Fucntion
+export const logout = () => {
+  signOut(auth);
+  localStorage.clear();
+
+  Cookies.remove("userID");
+  Cookies.remove("uat");
+};
